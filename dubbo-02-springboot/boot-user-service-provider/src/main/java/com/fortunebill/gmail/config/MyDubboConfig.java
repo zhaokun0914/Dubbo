2e07eaaee1ce9fa069e64899996862d88b646892
zhaokun <283798272@qq.com>
2022-03-17 09:25:31 +0800

    增加测试案例

--- dubbo-02-springboot/boot-user-service-provider/src/main/java/com/fortunebill/gmail/config/MyDubboConfig.java
+++ dubbo-02-springboot/boot-user-service-provider/src/main/java/com/fortunebill/gmail/config/MyDubboConfig.java
@@ -1,10 +1,11 @@
 package com.fortunebill.gmail.config;
 
-import com.alibaba.dubbo.config.ApplicationConfig;
-import com.alibaba.dubbo.config.ProtocolConfig;
-import com.alibaba.dubbo.config.RegistryConfig;
+import com.alibaba.dubbo.config.*;
+import com.fortunebill.gmail.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 /**
  * @author Kavin
@@ -16,7 +17,7 @@ public class MyDubboConfig {
     @Bean
     public ApplicationConfig applicationConfig() {
         ApplicationConfig applicationConfig = new ApplicationConfig();
-        applicationConfig.setName("boot-user-service-provider-bean");
+        applicationConfig.setName("boot-user-service-provider-api");
         return applicationConfig;
     }
 
@@ -32,8 +33,24 @@ public class MyDubboConfig {
     public ProtocolConfig protocolConfig() {
         ProtocolConfig protocolConfig = new ProtocolConfig();
         protocolConfig.setName("dubbo");
-        protocolConfig.setPort(20880);
+        protocolConfig.setPort(20881);
         return protocolConfig;
+    }
+
+    @Bean
+    public ServiceConfig<UserService> userServiceServiceConfig(UserService userService) {
+        ServiceConfig<UserService> serviceConfig = new ServiceConfig<>();
+        serviceConfig.setInterface(UserService.class);
+        serviceConfig.setRef(userService);
+        serviceConfig.setVersion("1.0.0");
+
+        // 配置每一个method的信息
+        MethodConfig methodConfig = new MethodConfig();
+        methodConfig.setName("getUserAddressList");
+        methodConfig.setTimeout(2000);
+        // 将method的设置保存到service配置中
+        serviceConfig.setMethods(Collections.singletonList(methodConfig));
+        return serviceConfig;
     }
 
 }
